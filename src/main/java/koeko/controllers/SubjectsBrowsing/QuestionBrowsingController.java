9c9e2e53f500ce17cbb6ac0402ce1355f8686898
maximerchrd <maximerchrd@gmail.com>
2018-05-20 21:52:57 +0400

    started implementing subjects browsing

--- src/main/java/koeko/controllers/SubjectsBrowsing/QuestionBrowsingController.java
+++ src/main/java/koeko/controllers/SubjectsBrowsing/QuestionBrowsingController.java
@@ -1,15 +1,27 @@
-package koeko.controllers;
+package koeko.controllers.SubjectsBrowsing;
 
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import koeko.controllers.CreateQuestionController;
+import koeko.database_management.DbTableSubject;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.ResourceBundle;
+import java.util.Vector;
+
+import static koeko.database_management.DbTableSubject.getAllSubjects;
 
 /**
  * Created by maximerichard on 13.03.18.
@@ -29,6 +41,12 @@ public class QuestionBrowsingController implements Initializable {
             }
         };
         new Thread(getIPTask).start();
+
+        //retrieve data from the db
+        Vector<String> subjects = DbTableSubject.getAllSubjects();
+
+        //build the subjects tree
+
     }
 
     public void refreshIP() {
@@ -42,5 +60,22 @@ public class QuestionBrowsingController implements Initializable {
             }
         };
         new Thread(getIPTask).start();
+    }
+
+    public void createSubject() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/CreateSubject.fxml"));
+        Parent root1 = null;
+        try {
+            root1 = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        CreateQuestionController controller = fxmlLoader.getController();
+        Stage stage = new Stage();
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initStyle(StageStyle.DECORATED);
+        stage.setTitle("Create a New Subject");
+        stage.setScene(new Scene(root1));
+        stage.show();
     }
 }
